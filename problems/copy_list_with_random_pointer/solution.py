dd5c30c9325e25aba5dcb3003df2f1244a63947b
Elaine <jojolily@gmail.com>
2026-05-19 23:25:07 +0000

    Sync LeetCode submission - Copy List with Random Pointer (python3)

---
+++ problems/copy_list_with_random_pointer/solution.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return head
+        #1:複製節點並插入原節點後面
+        curr = head
+        while curr:
+            copy = Node(curr.val)
+            copy.next = curr.next
+            curr.next = copy
+            curr = copy.next
+        #2:設定 random
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+        #3: 拆分 linked list
+        curr = head
+        copy_head = head.next
+        while curr:
+            copy = curr.next
+            curr.next = copy.next
+            if copy.next:
+                copy.next = copy.next.next
+            curr = curr.next
+        return copy_head
