d93dca1175106ca45f0e944a3d81822daad8d181
Elaine <jojolily@gmail.com>
2026-05-21 23:19:15 +0000

    Sync LeetCode submission - Height Checker (python3)

--- problems/height_checker/solution.py
+++ problems/height_checker/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         expected = sorted(heights)
-        k = 0
+        count = 0
         for i in range(len(heights)):
             if expected[i] != heights[i]:
-                k += 1
-        return k
+                count += 1
+        return count
