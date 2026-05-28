4a1ad9c8eba30acec861f3de5de3afdcd7670e47
Elaine <jojolily@gmail.com>
2026-05-28 02:45:17 +0000

    Sync LeetCode submission - Combinations (python3)

---
+++ problems/combinations/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        path = []
+        def backtrack(start):
+            if len(path) == k:
+                res.append(path[:])
+                return
+            for num in range(start, n + 1):
+                path.append(num)
+                backtrack(num + 1)
+                path.pop()
+        backtrack(1)
+        return res
