bb45a6306d6ab191de22b2fc031e57e7238efd1e
Elaine <jojolily@gmail.com>
2026-06-15 15:25:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (67.01%)

--- 0153-find-minimum-in-rotated-sorted-array/solution.py
+++ 0153-find-minimum-in-rotated-sorted-array/solution.py
@@ -3,8 +3,8 @@ class Solution:
         left, right = 0, len(nums) - 1
         while left < right:
             mid = (left + right) // 2
-            if nums[mid] > nums[right]:
+            if nums[mid] > nums[right]: # mid 在較大的那半段，minimum 必在右邊
                 left = mid + 1
-            else:
+            else:  # mid 可能就是 minimum，或在左半段
                 right = mid
         return nums[left]
