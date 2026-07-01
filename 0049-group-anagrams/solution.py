282fa17474a41b89ffedeb98156917bc40864987
Elaine <jojolily@gmail.com>
2026-07-01 23:27:37 +0000

    Sync LeetCode submission Runtime - 15 ms (42.35%), Memory - 23.9 MB (26.55%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -2,7 +2,8 @@ from collections import defaultdict
 from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = defaultdict(list)
+        groups = defaultdict(list)  
+        #defaultdict(list)第一次存取不存在的 key時，不會KeyError，直接自動建立空[]，所以不需要先判斷key是否存在。
         for s in strs:
             # key: 26 字母出現次數的 tuple，作為 canonical form
             count = [0] * 26
