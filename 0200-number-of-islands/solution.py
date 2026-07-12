a276a3d26a5e523abd790d7e6fb46e9cafe7d2d9
Elaine <jojolily@gmail.com>
2026-07-12 01:34:05 +0000

    Sync LeetCode submission Runtime - 239 ms (71.70%), Memory - 21.7 MB (36.60%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -4,19 +4,21 @@ class Solution:
             return 0
         rows, cols = len(grid), len(grid[0])
         count = 0
+        
         def dfs(r: int, c: int) -> None:
             # 邊界條件：出界或是水/已訪問
             if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
                 return   #停止迴圈
-            grid[r][c] = '0'
-            # 往四個方向擴散
+            grid[r][c] = '0' #標記成'0',淹沒
+            # 往四個方向擴散, 遞迴呼叫確認有沒有1
             dfs(r + 1, c)
             dfs(r - 1, c)
             dfs(r, c + 1)
             dfs(r, c - 1)
+            
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
                     count += 1
-                    dfs(r, c)
+                    dfs(r, c)  #四個方向都是0時才會再確認下一個位置
         return count
