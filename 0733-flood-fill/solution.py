e9c2146cc63fe36f210a0d215218b20cb1a35edf
Elaine <jojolily@gmail.com>
2026-07-14 08:02:00 +0000

    Sync LeetCode submission Runtime - 4 ms (4.98%), Memory - 19.9 MB (16.32%)

---
+++ 0733-flood-fill/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m, n = len(image), len(image[0])
+        start_color = image[sr][sc]
+        
+        if start_color == color:  #新色=舊色
+            return image
+        
+        def dfs(r: int, c: int) -> None:
+            if r < 0 or r >= m or c < 0 or c >= n:
+                return
+            if image[r][c] != start_color:
+                return
+            image[r][c] = color
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+            
+        dfs(sr, sc)
+        return image
+            
